9808b2294f0ad2932a92101d4124c41b3962e4ba
Johnny Garcia <johnnygar98@hotmail.com>
2020-04-02 08:56:02 -0500

    Renombrando bien la clase de Encabezado Factura

--- app/Models/EncabezadoFactura.php
+++ app/Models/EncabezadoFactura.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Encabezado_Factura extends Model
+class EncabezadoFactura extends Model
 {
     protected $table = 'encabezado_facturas';
 
