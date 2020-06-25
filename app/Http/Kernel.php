034b31f231f0668a54ede62df455fc5df25e2b15
Johnny Garcia <johnnygar98@hotmail.com>
2020-06-24 19:22:46 -0500

    Aumentando el numero maxio de request para que no se bloqueen.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:600,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Localization::class,
             \App\Http\Middleware\VerifyToken::class,
